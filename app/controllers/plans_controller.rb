b4559e07a63024ca50c5b100c88ea7c816972f9d
IkkanWatanabe <110508666+ichi-7@users.noreply.github.com>
2022-11-09 07:12:52 +0000

    save

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -6,6 +6,10 @@ class PlansController < ApplicationController
   def show
   end
   
+  def create
+    
+  end
+  
   def route
     @plan = Plan.new
   end
@@ -16,6 +20,23 @@ class PlansController < ApplicationController
   
   def confirm
     @plan = Plan.new
+    @start_day = params[:plan]['start_day(1i)'] + "年" + params[:plan]['start_day(2i)'] + "月" + params[:plan]['start_day(3i)'] + "日"
+    @end_day = params[:plan]['end_day(1i)'] + "年" + params[:plan]['end_day(2i)'] + "月" + params[:plan]['end_day(3i)'] + "日"
+    @meeting_place = params[:plan][:meeting_place]
+    @meeting_time = params[:plan]['meeting_time(4i)'] + "：" + params[:plan]['meeting_time(5i)']
+    @memo = params[:plan][:memo]
+    
+    names = params[:plan][:spot_name]
+    @spot_names = names.split(",")
+    gon.spot_names = @spot_names
+    
+    lats = params[:plan][:spot_lat]
+    @spot_lats = lats.split(",")
+    gon.spot_lats = @spot_lats
+    
+    lngs = params[:plan][:spot_lng]
+    @spot_lngs = lngs.split(",")
+    gon.spot_lngs = @spot_lngs
   end
   
 end
